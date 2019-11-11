6c6a0d59fae15c39c688f7dc6a1f80c425843b66
Gerard Cabarse <gerard.cabarse@gmail.com>
2019-11-11 16:21:03 +0900

    adjusting routes - users + lessons seeded

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,12 @@
 class BookingsController < ApplicationController
   def create
+    raise
     @user = current_user
     @lesson = Lesson.find(params[:id])
     @booking = Booking.new(booking_params)
     @booking.lesson = @lesson
-    @booking.save!
     if current_user
+      @booking.save!
       redirect_to user_path(@user)
     else
       redirect_to new_user_session_path
@@ -14,6 +15,7 @@ class BookingsController < ApplicationController
 
   private
 
-  def lesson_params
+  def booking_params
+    params.require(:booking).permit(:user_id, :lesson_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
 require 'faker'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Lesson.destroy_all
 
 puts "gettin dat seed"
+
+4.times do
+  User.create!(
+    :first_name => Faker::Name.first_name,
+    :last_name => Faker::Name.last_name,
+    :email => Faker::Internet.email,
+    :password => "secret"
+    )
+end
 
 10.times do
   Lesson.create!(
     :title => Faker::Hipster.sentence,
     :time => Faker::Time.forward(days: 5,  period: :evening, format: :long),
     :location => Faker::Games::ElderScrolls.region,
-    :topic => Faker::Coffee.blend_name
+    :topic => Faker::Coffee.blend_name,
+    :user_id => rand(1..4)
     )
 end
+
 
 puts "fulla dat seed"
