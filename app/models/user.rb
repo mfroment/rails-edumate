31507453bfa1addbe59ffd677d928cc9cc454c33
Gerard Cabarse <gerard.cabarse@gmail.com>
2019-11-11 15:00:20 +0900

    updated models + routes - migrations completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
+  has_many :lessons
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/:id', to: 'users#show'
+  get 'lessons/:id', to: 'lessons#show'
+  get 'lessons/', to: 'lessons#index'
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
