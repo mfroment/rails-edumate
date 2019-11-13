a5dbf03d1e572ca8b1d9f07845522a0a7322e20a
Maxime Froment <mfroment99@gmail.com>
2019-11-13 14:33:12 +0900

    Added past lessons and some existing bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,27 @@ def edumate_seed_photo_url(photo_file)
   "https://res.cloudinary.com/dnoaxk4lw/image/upload/edumate/seed/#{photo_file}.jpg"
 end
 
+def forward_time
+  Faker::Time.forward(days: 5,  period: :evening, format: :long)[0...-2]+"00"
+end
+
+def backward_time
+  Faker::Time.backward(days: 5,  period: :evening, format: :long)[0...-2]+"00"
+end
+
+def user_lookup(first_name)
+  User.where(first_name: first_name).first.id
+end
+
+def lesson_lookup(title)
+  Lesson.where(title: title).first.id
+end
+
 puts "Start seeding"
 
 # Destroy
 puts "Destroying Existing DB"
+Booking.destroy_all
 Lesson.destroy_all
 User.destroy_all
 
@@ -54,55 +71,64 @@ seed_lessons = [
     title: 'Yoga beginners/intermediate',
     topic: 'Health',
     user_first_name: 'Jiyoung',
-    photo_file: 'lessons-01'
+    photo_file: 'lessons-01',
+    time: forward_time
   },
   {
     title: 'Piano Lessons',
     topic: 'Music',
     user_first_name: 'Jiyoung',
-    photo_file: 'lessons-02'
+    photo_file: 'lessons-02',
+    time: forward_time
   },
   {
     title: 'Ruby Programming',
     topic: 'Programming',
     user_first_name: 'Gerard',
-    photo_file: 'lessons-03'
+    photo_file: 'lessons-03',
+    time: forward_time
   },
   {
     title: 'Physics',
     topic: 'Science',
     user_first_name: 'Tom',
-    photo_file: 'lessons-04'
+    photo_file: 'lessons-04',
+    time: forward_time
   },
   {
     title: 'Italian Cooking',
     topic: 'Cooking',
     user_first_name: 'Maxime',
-    photo_file: 'lessons-05'
+    photo_file: 'lessons-05',
+    time: forward_time
   },
   {
     title: 'Skiing',
     topic: 'Sports',
     user_first_name: 'Maxime',
-    photo_file: 'lessons-06'
+    photo_file: 'lessons-06',
+    time: forward_time
   },
   {
     title: 'Gymnastics',
     topic: 'Health',
     user_first_name: 'Tom',
-    photo_file: 'lessons-07'
+    photo_file: 'lessons-07',
+    time: forward_time
   },
   {
     title: 'Speaking in Public',
     topic: 'Presenting',
     user_first_name: 'Gerard',
-    photo_file: 'lessons-08'
+    photo_file: 'lessons-08',
+    time: backward_time
   },
   {
     title: 'Helicopter Piloting',
     topic: 'Transport',
     user_first_name: 'Tom',
-    photo_file: 'lessons-09'
+    photo_file: 'lessons-09',
+    time: backward_time
   },
 ]
 
@@ -110,12 +136,28 @@ seed_lessons.each do |lesson|
   l = Lesson.create!({
     title: lesson[:title],
     topic: lesson[:topic],
-    user_id: User.where(first_name: lesson[:user_first_name]).first.id,
+    user_id: user_lookup(lesson[:user_first_name]),
     remote_photo_url: edumate_seed_photo_url(lesson[:photo_file]),
     location: Faker::Nation.capital_city,
-    time: Faker::Time.forward(days: 5,  period: :evening, format: :long)
+    time: lesson[:time]
   })
   puts " Added #{l.title}"
+end
+
+# Booking
+puts "Seeding Booking"
+seed_bookings = [
+  { user_id: user_lookup('Maxime'), lesson_id: lesson_lookup('Helicopter Piloting') },
+  { user_id: user_lookup('Jiyoung'), lesson_id: lesson_lookup('Helicopter Piloting') },
+  { user_id: user_lookup('Gerard'), lesson_id: lesson_lookup('Helicopter Piloting') },
+  { user_id: user_lookup('Tom'), lesson_id: lesson_lookup('Speaking in Public') },
+  { user_id: user_lookup('Jiyoung'), lesson_id: lesson_lookup('Speaking in Public') },
+  { user_id: user_lookup('Tom'), lesson_id: lesson_lookup('Skiing') },
+]
+
+seed_bookings.each do |booking|
+  b = Booking.create!(booking)
+  puts " Added past booking"
 end
 
 puts "Finished seeding"
