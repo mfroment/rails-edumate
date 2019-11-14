08ca2161ddf8f65453713fe47c6664ec06932d14
Gerard Cabarse <gerard.cabarse@gmail.com>
2019-11-14 15:16:13 +0900

    Basic tab functionality and styling working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,5 +5,4 @@ import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
-
 initMapbox();
