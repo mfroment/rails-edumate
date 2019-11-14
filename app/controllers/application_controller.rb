417ce7db5ea09bd74c72e65fa2030a2a4bd2f6c5
Jiyoung Ko <jclareko@gmail.com>
2019-11-14 16:38:04 +0900

    sign in redirection fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,11 +22,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    if stored_location_for(resource_or_scope) == '/'
-      lessons_path
-    else
-      stored_location_for(resource_or_scope) || super
-    end
+    stored_location_for(resource_or_scope) || super
   end
 
 end
