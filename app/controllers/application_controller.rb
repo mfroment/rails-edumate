a7669c64046c4185496001243085968e158be3a5
jy-ko <53289659+jy-ko@users.noreply.github.com>
2019-11-14 16:24:24 +0900

    redirections fixed (#53)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -10,4 +11,22 @@ class ApplicationController < ActionController::Base
   # For additional in app/views/devise/registrations/edit.html.erb
     # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if stored_location_for(resource_or_scope) == '/'
+      lessons_path
+    else
+      stored_location_for(resource_or_scope) || super
+    end
+  end
+
 end
