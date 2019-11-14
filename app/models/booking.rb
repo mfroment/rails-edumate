26c6c628b5cb9621768011cde1f69d5c8f451b33
Maxime Froment <mfroment99@gmail.com>
2019-11-15 00:16:35 +0900

    Booking validation to prevent multi-booking & booking of class by own teacher. Fixed seeding accordingly

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+
+  validates :lesson, presence: true
+  validates :user, presence: true, uniqueness: { scope: :lesson }
+  validate :student_not_teacher
+
+  def student_not_teacher
+    errors.add(:user, "cannot book a lesson they teach") if user == lesson.user
+  end
 end
