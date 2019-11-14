fce5cc136612c0782217b17ad504e061c87f1c01
Jiyoung Ko <jclareko@gmail.com>
2019-11-14 17:57:55 +0900

    top-lessons added on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
+    @lessons = Lesson.all.sort_by { |lesson| lesson.time }
   end
 
 end
