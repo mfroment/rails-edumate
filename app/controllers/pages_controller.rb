7ae36c9ba0a4989a290423e1a80a17b25c4fd380
Maxime Froment <mfroment99@gmail.com>
2019-11-15 14:42:16 +0900

    Top lessons future (#87)
    * Top lessons are future lessons
    
    * Cosmetic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
-    @lessons = Lesson.all.sort_by { |lesson| lesson.time }
+    @lessons = Lesson.future
   end
-
 end
