2505e4b3eaab70ca347d6953c87b6e668a4aead7
Maxime Froment <mfroment99@gmail.com>
2019-11-15 10:30:37 +0900

    Implemented number and tense on lesson/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def user_full_name(user)
     "#{user.first_name} #{user.last_name}"
   end
+
+  def pluralize_with_no(word, count)
+    count.zero? ? "no #{word}" : "#{count} #{word.pluralize(count)}"
+  end
 end
